c06b0f8d1b5bfcfa592cebb649fe3d0d5d30da12
Arjun Raghavan <arjun24ster@gmail.com>
2019-02-13 20:33:16 +0530

    updated user model

--- technology/react/devconnector/routes/api/users.js
+++ technology/react/devconnector/routes/api/users.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../../models/User");
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "users work" }));
+
+// @route   GET api/users/register
+// @desc    Register user
+// @access  Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.send(400).json({ email: "Email already exists" });
+    } else {
+    }
+  });
+});
 
 module.exports = router;
