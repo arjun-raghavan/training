dd54676dc7df6b3a146347a9ba5a8bf01ccd1e86
Arjun Raghavan <arjun24ster@gmail.com>
2019-02-24 20:58:17 +0530

    POST api/users/login

--- technology/react/devconnector/routes/api/users.js
+++ technology/react/devconnector/routes/api/users.js
@@ -46,4 +46,29 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route   POST api/users/login
+// @desc    Login user / Returning JWT Token
+// @access  Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    //check for user
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    //Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        return res.json({ msg: "success" });
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
