4e18ba3ec554ee0f8b57f218b016fe2862086f08
Arjun Raghavan M <arjun24ster@gmail.com>
2019-03-27 10:23:17 +0530

    ThemedButton example

--- technology/react/tutorial/react-basics/src/index.js
+++ technology/react/tutorial/react-basics/src/index.js
@@ -3,5 +3,7 @@ import React from "react";
 //To render a react component to a DOM
 import ReactDOM from "react-dom";
 import RoutesApp from "./app/RoutesApp/RoutesApp";
+import ContextApiApp from "./app/ContextApiApp/ContextApiApp";
 
-ReactDOM.render(<RoutesApp />, document.getElementById("root"));
+//ReactDOM.render(<RoutesApp />, document.getElementById("root"));
+ReactDOM.render(<ContextApiApp />, document.getElementById("root"));
