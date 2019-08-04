5bc3ee989d6024d4dbfae84c4be4df86731ef825
Arjun Raghavan <arjun24ster@gmail.com>
2019-08-04 21:28:54 +0530

    initial commit for user-management

---
+++ technology/microservices/microservices_income_tax_platform/user-management/src/main/java/com/perseverance/usermanagement/configuration/AuditingConfiguration.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.perseverance.usermanagement.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * @author Arjun
+ *
+ */
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfiguration {
+
+}
