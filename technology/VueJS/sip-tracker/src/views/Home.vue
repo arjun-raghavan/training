dcd26cc6b9dc4beafd383447666bc889b452d4e1
arjun-raghavan <arjun24ster@gmail.com>
2020-06-28 14:28:42 +0530

    summary page

--- technology/VueJS/sip-tracker/src/views/Home.vue
+++ technology/VueJS/sip-tracker/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <MfSummary/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +9,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import MfSummary from '@/components/MfSummary.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    MfSummary,
   },
 };
 </script>
