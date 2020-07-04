74a830c655ab06e05ee62587179598d7cae189dc
arjun-raghavan <arjun24ster@gmail.com>
2020-07-04 16:00:46 +0530

    code clean up

--- technology/VueJS/sip-tracker/src/components/Fund.vue
+++ technology/VueJS/sip-tracker/src/components/Fund.vue
@@ -7,58 +7,9 @@
 <script>
 
 export default {
-  computed: {
-    portfolio() {
-      return this.$store.getters.myPortfolio;
-    },
-  },
-  methods: {
-    convertToFixedDecimal(value) {
-      return Number(value.toFixed(2));
-    },
-    currentValue(fund) {
-      const history = this.$store.getters.navHistory;
-      const fundNav = history.find(
-        (nav) => nav.name === fund.name,
-      );
 
-      if (fundNav) {
-        return this.convertToFixedDecimal(fund.units * fundNav.curentNav);
-      }
-      return this.convertToFixedDecimal(fund.units);
-    },
-    profit(fund) {
-      return this.convertToFixedDecimal((fund.units * fund.nav) - fund.investment);
-    },
-    XIRR(fund) {
-      return `${this.convertToFixedDecimal((this.profit(fund) / fund.investment) * 100)} %`;
-    },
-  },
 };
 </script>
 
 <style scoped>
-#portfolio {
-  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
-
-#portfolio td, #portfolio th {
-  border: 1px solid #ddd;
-  padding: 8px;
-  text-align: left;
-}
-
-#portfolio tr:nth-child(even){background-color: #f2f2f2;}
-
-#portfolio tr:hover {background-color: #ddd;}
-
-#portfolio th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #4CAF50;
-  color: white;
-}
 </style>
