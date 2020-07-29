66768a5d0847bb61b9fe1b723462a3616e656aec
arjun-raghavan <arjun24ster@gmail.com>
2020-07-29 11:30:17 +0530

    component deeep dive section

---
+++ technology/VueJS/course/02_dev_server-setup/01_vue_cli/src/component-deep-dive/AppComponentDeepDive.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-servers></app-servers>
+            <ServerDetails></ServerDetails>
+        </div>
+        <hr>
+        <appFooter></appFooter>
+    </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import ServerList from "./components/ServerList.vue";
+import ServerDetails from "./components/ServerDetails.vue";
+
+export default {
+    components: {
+        appHeader: Header,
+        appFooter: Footer,
+        'app-servers': ServerList,
+        ServerDetails
+    }
+}
+
+</script>
+
+<style>
+
+</style>
--- technology/VueJS/course/02_dev_server-setup/01_vue_cli/src/main.js
+++ technology/VueJS/course/02_dev_server-setup/01_vue_cli/src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 // import App from './App.vue'
-import App01 from './App01.vue';
+// import App01 from './App01.vue';
 import ServerState from './components/ServerState';
+import AppComponentDeepDive from './component-deep-dive/AppComponentDeepDive';
 
 Vue.component('app-servers', ServerState);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App01),
+  render: h => h(AppComponentDeepDive),
 }).$mount('#app')
