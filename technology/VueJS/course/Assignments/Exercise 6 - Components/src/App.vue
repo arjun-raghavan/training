6701477a71cb3f6f4ded8e07827a214b6c1c07d3
arjun-raghavan <arjun24ster@gmail.com>
2020-07-29 11:14:06 +0530

    Assginment 6 - components

---
+++ technology/VueJS/course/Assignments/Exercise 6 - Components/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <Header></Header>
+        <hr>
+        <div class="row">
+            <ServerList></ServerList>
+            <ServerDetails></ServerDetails>
+        </div>
+        <hr>
+        <Footer></Footer>
+    </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import ServerList from "./components/ServerList.vue";
+import ServerDetails from "./components/ServerDetails.vue";
+
+export default {
+    components: {
+        Header,
+        Footer,
+        ServerList,
+        ServerDetails
+    }
+}
+
+</script>
+
+<style>
+
+</style>
