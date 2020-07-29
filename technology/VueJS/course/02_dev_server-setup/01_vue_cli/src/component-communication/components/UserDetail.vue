377b888c6ce27137fabce87a8bcc03421cfffe4f
arjun-raghavan <arjun24ster@gmail.com>
2020-07-29 12:00:23 +0530

    props validation

---
+++ technology/VueJS/course/02_dev_server-setup/01_vue_cli/src/component-communication/components/UserDetail.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ name }}</p>
+        <p>Reversed Name: {{ reverseName() }}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        props: {
+            // name: String
+            // name: [String, Array]
+            name: {
+                type: String,
+                required: true
+                // default: 'Test' // provide default is required is not true
+            }
+        },
+        methods: {
+            reverseName(){
+                return this.name.split("").reverse().join("");
+            }
+        }     
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral !important;
+    }
+</style>
--- technology/VueJS/course/02_dev_server-setup/01_vue_cli/src/main.js
+++ technology/VueJS/course/02_dev_server-setup/01_vue_cli/src/main.js
@@ -2,12 +2,13 @@ import Vue from 'vue'
 // import App from './App.vue'
 // import App01 from './App01.vue';
 import ServerState from './components/ServerState';
-import AppComponentDeepDive from './component-deep-dive/AppComponentDeepDive';
+// import AppComponentDeepDive from './component-deep-dive/AppComponentDeepDive';
+import AppComponentCommunication from "./component-communication/AppComponentCommunication";
 
 Vue.component('app-servers', ServerState);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(AppComponentDeepDive),
+  render: h => h(AppComponentCommunication),
 }).$mount('#app')
