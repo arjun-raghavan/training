b8054a49d0b3dbb96d574aa796a69dbc0a1637cb
arjun-raghavan <arjun24ster@gmail.com>
2020-07-31 13:41:33 +0530

    vue resouce example

--- technology/VueJS/course/02_dev_server-setup/02_vue_resource_route_store/src/main.js
+++ technology/VueJS/course/02_dev_server-setup/02_vue_resource_route_store/src/main.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+// import App from './App.vue'
+// import router from './router'
+// import store from './store'
 import VueResource from 'vue-resource'
+import AppHttpVueResource from './vue-resource/AppHttpVueResource'
 
 Vue.use(VueResource)
+Vue.http.options.root = ''
+
+// interceptors - array of functions
+Vue.http.interceptors.push((request, next) => {
+  // if (request.method === 'POST') {
+  //   request.method = 'PUT'
+  // }
+  console.log(request)
+  next(response => {
+    console.log(response)
+    // response.json = () => { return { messages: response.body } }
+  })
+})
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+  // router,
+  // store,
+  render: h => h(AppHttpVueResource)
 }).$mount('#app')
--- technology/VueJS/course/02_dev_server-setup/02_vue_resource_route_store/src/vue-resource/AppHttpVueResource.vue
+++ technology/VueJS/course/02_dev_server-setup/02_vue_resource_route_store/src/vue-resource/AppHttpVueResource.vue
@@ -2,40 +2,100 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>https://apphttpvueresource.firebaseio.com/</h1>
+                <h1></h1>
 
                 <div class="form-group">
                     <label>Username</label>
                     <input class="form-control" type="text" v-model="user.username">
                 </div>
-                
+
                 <div class="form-group">
                     <label>Mail</label>
                     <input class="form-control" type="text" v-model="user.email">
                 </div>
 
-                <button class="btn btn-primary" @click="submit">Submit</button>
+                <button type="button" class="btn btn-primary" @click="submit">Submit</button>
+                <button type="button" class="btn btn-warning" @click="save">Resource Save</button>
+                <button type="button" class="btn btn-danger" @click="saveAction">Save Alt - Action</button>
+                <hr>
+                 <div class="form-group">
+                    <label>Node</label>
+                <input class="form-control" type="text" v-model="node">
+                </div>
+                <br>
+                <button type="button" class="btn btn-primary" @click="fetchData">Fetch Data</button>
+                <button type="button" class="btn btn-primary" @click="fetchDataNode">Fetch Data Node</button>
+                <br>
+                <ul>
+                    <li class="list-group-item" v-for="user in users" :key="user.email">{{user.username}}</li>
+                </ul>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        data(){
-            return{
-                user: {
-                    username: '',
-                    email: ''
-                }
-            }
-        },
-        methods: {
-            submit(){
-                console.log(this.user);
-            }
-        }
+export default {
+  data () {
+    return {
+      user: {
+        username: '',
+        email: ''
+      },
+      users: [],
+      resource: {},
+      node: 'data'
     }
+  },
+  methods: {
+    save () {
+      this.resource.save({}, this.user)
+    },
+    saveAction () {
+      this.resource.saveAlt(this.user)
+    },
+    submit () {
+    //   this.$http.post('', this.user)
+      this.$http.post('data.json', this.user)
+        .then(response => { console.log(response) }, error => { console.log(error) })
+    },
+    fetchData () {
+    //   this.$http.get('')
+      this.$http.get('data.json')
+        .then(response => {
+          return response.json()
+        }).then(data => {
+          console.log(data)
+          const resultArray = []
+          for (const key in data) {
+            resultArray.push(data[key])
+          }
+          this.users = resultArray
+          console.log(this.users)
+        })
+    },
+    fetchDataNode () {
+      this.resource.getData({ node: this.node })
+        .then(response => {
+          return response.json()
+        }).then(data => {
+          const resultArray = []
+          for (const key in data) {
+            resultArray.push(data[key])
+          }
+          this.users = resultArray
+        })
+    }
+  },
+  created () {
+    const customActions = {
+      saveAlt: { method: 'POST', url: 'alt.json' },
+      getData: { method: 'GET' }
+    }
+    // this.resource = this.$resource('data.json', {}, customActions)
+    this.resource = this.$resource('{node}.json', {}, customActions)
+  }
+}
 </script>
 
 <style>
