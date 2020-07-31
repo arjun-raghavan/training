4c7e4a2293619f5a7a92b0f4f991231f22d673a1
Arjunraghavan Manathanath <Arjunraghavan.Manathanath@nike.com>
2020-07-31 18:45:35 +0530

    scroll #

--- technology/VueJS/course/02_dev_server-setup/03_routing_example/src/components/user/UserDetail.vue
+++ technology/VueJS/course/02_dev_server-setup/03_routing_example/src/components/user/UserDetail.vue
@@ -3,12 +3,43 @@
     <h3>Some User Details</h3>
     <p>User with id : {{ $route.params.id}} loaded</p>
     <!-- //:to="'/user/'+ $route.params.id + '/edit'" -->
+    <!--
     <router-link 
         tag="button" 
         :to="{name: 'userEdit', params: {id:$route.params.id}, query: {locale: 'en', q: 100 }}"
         class="btn btn-primary">Edit User</router-link>
+    -->
+    <router-link
+        tag="button"
+        :to="link"
+        class="btn btn-primary">Edit User
+    </router-link>
     </div>
 </template>
 
 <script>
+    export default {
+        data() {
+            return {
+                link: {
+                    name: 'userEdit',
+                    params: {
+                        id: this.$route.params.id
+                    },
+                    query: {
+                        locale: 'en',
+                        q: 100
+                    },
+                    hash: '#data'
+                }
+            }
+        },
+        beforeRouteEnter(to, from, next) {
+            if (true) {
+                next();
+            } else {
+                next(false);
+            }
+        }
+    }
 </script>
