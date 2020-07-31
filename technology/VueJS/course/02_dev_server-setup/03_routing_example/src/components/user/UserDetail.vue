337dbac1ac59ea364015ab34620327f1ea458c2e
Arjunraghavan Manathanath <Arjunraghavan.Manathanath@nike.com>
2020-07-31 16:39:33 +0530

    routes

--- technology/VueJS/course/02_dev_server-setup/03_routing_example/src/components/user/UserDetail.vue
+++ technology/VueJS/course/02_dev_server-setup/03_routing_example/src/components/user/UserDetail.vue
@@ -1,3 +1,13 @@
 <template>
+    <div>
     <h3>Some User Details</h3>
+    <p>User with id : {{ $route.params.id}} loaded</p>
+    <router-link 
+        tag="button" 
+        :to="'/user/'+ $route.params.id + '/edit'"
+        class="btn btn-primary">Edit User</router-link>
+    </div>
 </template>
+
+<script>
+</script>
