dcce8cb39095c8a4a56d0263526797fdb712f094
Arjunraghavan Manathanath <Arjunraghavan.Manathanath@nike.com>
2020-07-31 15:59:16 +0530

    route issu fix

--- technology/VueJS/course/02_dev_server-setup/03_routing_example/src/components/Header.vue
+++ technology/VueJS/course/02_dev_server-setup/03_routing_example/src/components/Header.vue
@@ -5,6 +5,7 @@
       <li role="presentation"><router-link to="/user">User</router-link></li> -->
       <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
       <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
+      <router-link to="/profile" tag="li" active-class="active"><a>Profile</a></router-link>
     </ul>
   </div>
 </template>
--- technology/VueJS/course/02_dev_server-setup/03_routing_example/src/routes.js
+++ technology/VueJS/course/02_dev_server-setup/03_routing_example/src/routes.js
@@ -1,15 +1,17 @@
-import User from "./components/user/User.vue"
-import UserStart from "./components/user/UserStart.vue"
-import UserDetail from "./components/user/UserDetail.vue"
-import UserEdit from "./components/user/UserEdit.vue"
+import User from "./components/user/User.vue";
+import UserStart from "./components/user/UserStart.vue";
+import UserDetail from "./components/user/UserDetail.vue";
+import UserEdit from "./components/user/UserEdit.vue";
 import Home from "./components/Home.vue";
+import Profile from "./components/Profile.vue";
 
 export const routes = [
   // {path: '/user/:id', component: User},
   {path: '/user', component: User, children: [
-    {path: "", components: UserStart},
-    {path: ":id", components: UserDetail},
-    {path: ":id/edit", components: UserEdit}
+    {path: "", component: UserStart},
+    {path: ":id", component: UserDetail},
+    {path: ":id/edit", component: UserEdit}
   ]},
-  {path: '', component: Home}
+  {path: '', component: Home},
+  {path: '/profile', component: Profile},
 ]
