3ba3115422c42b5ec6eb8846c22b183ad36aa7ec
Arjunraghavan Manathanath <Arjunraghavan.Manathanath@nike.com>
2020-08-03 21:21:37 +0530

    vuex

---
+++ technology/VueJS/course/02_dev_server-setup/06-vuex/src/components/Counter.vue
@@ -0,0 +1,61 @@
+<template>
+<div>
+    <div>
+        <button class="btn btn-primary" @click="increment(100)">Increment</button>
+        <button class="btn btn-primary" @click="decrement(100)">Decrement</button>
+    </div>
+    <div>
+        <button class="btn btn-success" @click="asyncIncrement({by: 50, duration: 500})">Increment</button>
+        <button class="btn btn-success" @click="asyncDecrement({by: 50, duration: 500})">Decrement</button>
+    </div>
+</div>
+</template>
+
+<script>
+    //import {mapMutations} from 'vuex';
+    import {mapActions} from 'vuex';
+
+    export default {
+        /*Without mapMutations
+        methods: {
+            increment() {
+                // this.$emit('updated', 1);
+                // this.$store.state.counter++;
+                this.$store.commit('increment');
+            },
+            decrement() {
+                // this.$emit('updated', -1);
+                // this.$store.state.counter--;
+                this.$store.commit('decrement');
+            }
+        }
+        */
+        /* using mapMutations
+        methods: {
+            ...mapMutations([
+                'increment',
+                'decrement'
+            ]),
+            ownMethod(){
+                return "ownMethod";
+            }
+        }
+        */
+        //mapActions
+        //What mapActions does
+        // increment() { this.$store.dispatch('increment)} 
+        methods: {
+            ...mapActions([
+                'increment',
+                'decrement',
+                'asyncIncrement',
+                'asyncDecrement'
+            ]),
+            ownMethod(){
+                return "ownMethod";
+            }
+
+        }
+        
+    }
+</script>
---
+++ technology/VueJS/course/02_dev_server-setup/06-vuex/src/components/Result.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+    
+    <p>Double Counter is: {{ doubleCounter }}</p>
+    <p>Clicks: {{ stringCounter }}</p>
+    <p>ownComputedProp: {{ ownComputedProp }}</p>
+    
+</div>
+</template>
+
+<script>
+    import {mapGetters} from 'vuex';
+
+    export default {
+        // props: ['counter']
+        /**
+        computed: {
+
+            counter(){
+                return this.$store.state.counter;
+            },
+            doubleCounter(){
+                return this.$store.getters.doubleCounter;
+            },
+            stringCounter(){
+                return this.$store.getters.stringCounter;
+            }
+        }
+        **/
+        /**
+        computed: mapGetters([
+            'doubleCounter',
+            'stringCounter'
+        ])
+        **/
+        // Adding additional computed properties
+        computed: {
+            ...mapGetters([
+            'doubleCounter',
+            'stringCounter'
+            ]),
+            ownComputedProp(){
+                return 0;
+            }
+        }
+    }
+</script>
