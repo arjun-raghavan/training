39460442c9c9febd3cd917ad9e873e7b87e3138e
Arjunraghavan Manathanath <Arjunraghavan.Manathanath@nike.com>
2021-02-18 19:39:17 +0100

    code using community version

--- technology/ag-grid/03-vue2-basic/src/App.vue
+++ technology/ag-grid/03-vue2-basic/src/App.vue
@@ -1,32 +1,49 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <ag-grid-vue
+    style="width: 500px; height: 500px"
+    class="ag-theme-alpine"
+    :columnDefs="columnDefs"
+    :rowData="rowData"
+    rowSelection="multiple"
+  >
+  </ag-grid-vue>
 </template>
 
+<script>
+import { AgGridVue } from "ag-grid-vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      columnDefs: null,
+      rowData: null,
+    };
+  },
+  components: {
+    AgGridVue,
+  },
+  beforeMount() {
+    this.columnDefs = [
+      { field: "make", sortable: true, filter: true, checkboxSelection: true },
+      { field: "model", sortable: true, filter: true },
+      { field: "price", sortable: true, filter: true },
+    ];
+
+    fetch("https://www.ag-grid.com/example-assets/row-data.json")
+      .then((result) => result.json())
+      .then((rowData) => (this.rowData = rowData));
+
+    // this.rowData = [
+    //   { make: "Toyota", model: "Celica", price: 35000 },
+    //   { make: "Ford", model: "Mondeo", price: 32000 },
+    //   { make: "Porsche", model: "Boxter", price: 72000 },
+    // ];
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
+@import "../node_modules/ag-grid-community/dist/styles/ag-theme-alpine.css";
 </style>
